f732f9a4942635d7e2e3884b393200c8061293a3
l-proger <l-proger@yandex.ru>
2023-01-09 02:44:45 +0300

    *

--- F7/Interfaces/MicroNetwork.Task.MemoryAccess.HostDeviceInterop.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-//imported type: std::uint32_t from module: cstdint
-//imported type: LFramework::IUnknown from module: LFramework/COM/ComObject.h
-//imported type: void from module: LanguagePrimitive
-//imported type: bool from module: LanguagePrimitive
-#include <LFramework/COM/ComObject.h>
-#include <cstdint>
-
